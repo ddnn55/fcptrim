0e4583c1a3fbb435f5dcc3ad653e56556beba0ea
David Stolarsky <david.stolarsky@gmail.com>
2016-05-08 13:43:10 -0400

    re-encode; otherwise, not frame-accurate :/

--- fcptrim.js
+++ fcptrim.js
@@ -6,31 +6,35 @@ var fs = require('fs'),
     assert = require('assert'),
     path = require('path');
 
+var trimmedDir = 'trimmed-more';
+
 console.log('# pipe this to bash! or save it for later.');
 
-var clip = path.basename(argv._[0], '.xml');
-console.log('mkdir -p ', path.join('trimmed', clip));
+argv._.forEach(function(fcpXmlPath) {
+	var clip = path.basename(fcpXmlPath, '.xml');
+	console.log('mkdir -p ', path.join(trimmedDir, clip));
 
-fs.readFile(argv._[0], {encoding:'utf8'}, function(err, data) {
-	xml2js.parseString(data, function(err, project) {
+	fs.readFile(fcpXmlPath, {encoding:'utf8'}, function(err, data) {
+		xml2js.parseString(data, function(err, project) {
 
-		var duration = +project.xmeml.sequence[0].duration[0];
+			var duration = +project.xmeml.sequence[0].duration[0];
 
-		project.xmeml.sequence[0].media[0].video[0].track.forEach(function(track) {
-			
-			var filePath = decodeURI(track.clipitem[0].file[0].pathurl[0].slice('file://localhost'.length));
-			var _in = +track.clipitem[0].in[0];
-			var out = +track.clipitem[0].out[0];
-			var timebase = +track.clipitem[0].rate[0].timebase[0];
-			
-			assert.equal(0, +track.clipitem[0].start[0]);
-			assert.equal(duration, out-_in);
+			project.xmeml.sequence[0].media[0].video[0].track.forEach(function(track) {
+				
+				var filePath = decodeURI(track.clipitem[0].file[0].pathurl[0].slice('file://localhost'.length));
+				var _in = +track.clipitem[0].in[0];
+				var out = +track.clipitem[0].out[0];
+				var timebase = +track.clipitem[0].rate[0].timebase[0];
+				
+				assert.equal(0, +track.clipitem[0].start[0]);
+				assert.equal(duration, out-_in);
 
-			var inSeconds = _in / timebase;
+				var inSeconds = _in / timebase;
 
-			var filename = path.basename(filePath);
+				var filename = path.basename(filePath, '.MP4');
 
-			console.log('avconv -i "' + filePath + '" -ss ' + inSeconds + ' -t ' + duration / timebase + ' -codec copy ' + path.join('trimmed', clip, filename));
+				console.log('avconv -i "' + filePath + '" -ss ' + inSeconds + ' -t ' + duration / timebase + ' -vcodec libx264 -preset veryfast -crf 18 -acodec copy ' + path.join(trimmedDir, clip, filename) + '.mp4');
+			});
 		});
 	});
 });
